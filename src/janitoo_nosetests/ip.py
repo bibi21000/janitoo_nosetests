9196113ae39cc960a9328b62da3bef822d016fcf
bibi21000 <bibi21000@gmail.com>
2015-12-28 19:24:55 +0100

    Add ip test framework

---
+++ src/janitoo_nosetests/ip.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+__license__ = """
+
+This file is part of **janitoo** project https://github.com/bibi21000/janitoo.
+
+License : GPL(v3)
+
+**janitoo** is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+**janitoo** is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with janitoo. If not, see http://www.gnu.org/licenses.
+"""
+__copyright__ = "Copyright © 2013-2014 Sébastien GALLET aka bibi21000"
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except:  # pragma: no cover
+    # bootstrapping
+    pass # pragma: no cover
+
+import sys, os, errno
+import time
+import unittest
+import socket;
+from janitoo_nosetests import JNTTBase
+
+
+class JNTTPort(JNTTBase):
+    """Port base test
+    """
+
+    def assertTCP(self, port, ip='127.0.0.1'):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ip,port))
+        self.assertTrue(result == 0)
+
